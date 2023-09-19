5e11951f15bc3df41b4143ebb331bace42a0ff5e
LighthouseKeeperYN <freesaber@yandex.ru>
2023-09-19 12:20:31 +0200

    feat: setup project

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    entry: "./src/main.ts",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "main.js",
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    target: "web",
+    node: {
+      __dirname: false,
+    },
+    mode: "development",
+    devServer: {
+      devMiddleware: {
+        writeToDisk: true,
+      },
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+  },
+];
